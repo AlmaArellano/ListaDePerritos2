86b19483345c30a38b6190dcd06793aafb7e4084
Alma Arellano <alma.arellano8936@alumnos.udg.mx>
2023-12-07 18:56:23 -0600

    Initial commit

---
+++ app/src/main/java/arellano/alma/listadeperritos2/APIService.kt
@@ -0,0 +1,10 @@
+package arellano.alma.listadeperritos2
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface APIService {
+    @GET
+    suspend fun getDogsByBreeds(@Url url:String):Response<DogsResponse>
+}
